75ea63717cb825e8cbe63914af44ba16feb00780
maxcore25 <mkorobov2002@mail.ru>
2022-04-26 23:43:01 +0300

    Query Client Options Added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
