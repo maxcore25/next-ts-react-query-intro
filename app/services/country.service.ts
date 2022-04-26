0f1d6b5df6b2302e8fd49ba14fcb3fecb81547db
maxcore25 <mkorobov2002@mail.ru>
2022-04-26 23:51:27 +0300

    Endpoint Added

--- app/services/country.service.ts
+++ app/services/country.service.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3001';
 
+axios.defaults.baseURL = API_URL;
+
 export const CountryService = {
   async getAll() {
-    return axios.get('');
+    return axios.get('/countries');
   },
 };
