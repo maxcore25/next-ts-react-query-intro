ab896f92e09a59fe4731d1df9d5f34a4360b2325
maxcore25 <mkorobov2002@mail.ru>
2022-04-29 10:38:53 +0300

    Country Type Interface Written

--- app/services/country.service.ts
+++ app/services/country.service.ts
@@ -4,8 +4,14 @@ const API_URL = 'http://localhost:3001';
 
 axios.defaults.baseURL = API_URL;
 
+export interface ICountry {
+  id: number;
+  name: string;
+  description: string;
+}
+
 export const CountryService = {
   async getAll() {
-    return axios.get('/countries');
+    return axios.get<ICountry[]>('/countries');
   },
 };
