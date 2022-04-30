37442278b805a4356598d7f7d2ea0ed689e4a3b4
maxcore25 <mkorobov2002@mail.ru>
2022-04-30 10:29:27 +0300

    Dynamic Country Page Done

--- app/hooks/useCountry.tsx
+++ app/hooks/useCountry.tsx
@@ -1,16 +1,17 @@
 import { useQuery } from 'react-query';
 import { CountryService, ICountry } from '../services/country.service';
 
-export const useCountry = (id: string) => {
+export const useCountry = (id?: string) => {
   const {
     isLoading,
     error,
     data: country,
-  } = useQuery(['county list', id], () => CountryService.getById(id), {
+  } = useQuery(['county list', id], () => CountryService.getById(id || ''), {
     onError: (error: any) => {
       alert(error.message);
     },
     select: ({ data }): ICountry => data,
+    enabled: !!id,
   });
   return { isLoading, country, error };
 };
